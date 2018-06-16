96b6abf2cf2f0bd451d700cb3bf15c15c839011e
kwamesarpong <kayosarp@gmail.com>
2018-06-16 19:32:10 +0000

    Installed and Linked all libraries for android

--- android/app/src/main/java/com/rosefabrics/MainApplication.java
+++ android/app/src/main/java/com/rosefabrics/MainApplication.java
@@ -3,6 +3,8 @@ package com.rosefabrics;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new ImagePickerPackage(),
             new ReactNativeOneSignalPackage(),
             new SplashScreenReactPackage()
       );
